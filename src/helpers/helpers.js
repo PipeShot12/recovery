db3f358fcfffcb4b51f5a029e8ec37e378fdb394
Andres <andres1295zz@gmail.com>
2022-08-05 19:53:10 -0500

    second complex post enabled read only fields 2

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -106,6 +106,7 @@ async function generatePdf(obj, index, arrayLength) {
     const mes = form.getTextField('MES')
     const dia = form.getTextField('DIA')
     const razon = form.getTextField('RAZON EMPRESA')
+    const nit = form.getTextField('NIT EMPRESA')
     const correo = form.getTextField('CORREO')
     const tipoAf = form.getTextField('PENSION')
     const inicial = form.getTextField('TIPO')
